3013678f3c92f3b5e35e2d57ddc97df33b3f0d6f
nishanth <gayatridipali@gmail.com>
2021-04-17 19:33:04 +0530

    fixed email url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const signup_email_api_url = __prod__
-  ? "https://goloop-micro-auth.vercel.app/api/signup"
+  ? "https://goloop-micro-email.vercel.app/api/signup"
   : "http://localhost:3003/api/signup";
